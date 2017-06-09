b3886deb75520c598bd508a75de883889669df8f
jrzollin <jrzollin@yahoo.com>
2017-06-08 20:36:12 -0700

    switched java link to sales.  working calculation of cookies per hour.

--- app.js
+++ app.js
@@ -4,14 +4,24 @@ function customerNumber(min, max){
   return Math.floor(Math.random() * ((max + 1) - min)) + min;
 }
 
+function cookiesPerHour(store){
+  for(var i = 0; i<store.hoursOpen.length; i++){
+    store.averageCustomerHour = customerNumber(store.minCustomersHour, store.maxCustomersHour);
+    console.log('Hour ' + (i + 1) + ' customers = ' + store.averageCustomerHour);
+    var cookies = (store.averageCustomerHour * store.averageCustomerCookies);
+    store.cookiesPurchasedHour.push(cookies);
+    console.log('Hour ' + (i + 1) + ' cookies purchased = ' + store.cookiesPurchasedHour[i]);
+  }
+}
 
 var pikeStore = {
   minCustomersHour: 23,
   maxCustomersHour: 65,
   averageCustomerHour: 0,
   averageCustomerCookies: 6.3,
-  cookiesPurchasedHour: []
+  cookiesPurchasedHour: [],
+  hoursOpen: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm']
 }
 
-pikeStore.averageCustomerHour = customerNumber(pikeStore.minCustomersHour, pikeStore.maxCustomersHour);
-console.log(pikeStore.averageCustomerHour);
+
+cookiesPerHour(pikeStore);
